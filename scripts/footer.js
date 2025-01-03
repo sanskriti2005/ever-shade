7b81206126e02a73398b6f8061e9fef778de6e39
Sanskriti <sanskritisri27@gmail.com>
2025-01-03 07:03:04 +0530

    added scroll feature to the bottomost set of cardss

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,6 +1,9 @@
 function loadFooter(){
     // catch the navbar div element
     const footer = document.querySelector("#footer-div");
-    footer.innerHTML = ``
+    footer.innerHTML = `<div>Website feedback? Let us know</div>
+        <div id="footer">
+            <p>&copy;2024 EverShade All Rights Reserved</p>
+        </div>`
 }
 loadFooter();
